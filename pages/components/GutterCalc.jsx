07b74b97dc0a8d2ac23cedb4e517403843b23ee0
Jacob Benson <benson.b.jacob@gmail.com>
2022-10-25 14:50:29 -0500

    Add ability to edit cost per item.

--- pages/components/GutterCalc.jsx
+++ pages/components/GutterCalc.jsx
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 
 const GutterCalc = () => {
   const [linear, setLinear] = useState(0);
+  const [costLinear, setCostLinear] = useState(0);
   const [rightEnd, setRightEnd] = useState(0);
+  const [costRight, setCostRight] = useState(0);
   const [leftEnd, setLeftEnd] = useState(0);
+  const [costLeft, setCostLeft] = useState(0);
   const [insideCorner, setInsideCorner] = useState(0);
+  const [costInner, setCostInner] = useState(0);
   const [outsideCorner, setOutsideCorner] = useState(0);
+  const [costOuter, setCostOuter] = useState(0);
   const [total, setTotal] = useState(0);
 
   let calcTotal = (event) => {
@@ -18,11 +23,11 @@ const GutterCalc = () => {
       );
     } else {
       let total =
-        linear * 2.5 +
-        rightEnd * 1 +
-        leftEnd * 1 +
-        insideCorner * 2 +
-        outsideCorner * 2;
+        linear * costLinear +
+        rightEnd * costRight +
+        leftEnd * costLeft +
+        insideCorner * costInner +
+        outsideCorner * costOuter;
       setTotal(total.toFixed(1));
     }
   };
@@ -40,64 +45,118 @@ const GutterCalc = () => {
 
       <div className='bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl'>
         <div className='mt-4'>
-          <form className='flex flex-col' onSubmit={calcTotal}>
-            <div className='mb-6 pt-3 rounded bg-gray-200'>
-              <label className='block text-gray-700 text-sm font-bold mb-2 ml-3'>
-                Linear Feet
-              </label>
-              <input
-                className='bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3'
-                value={linear}
-                onChange={(e) => setLinear(e.target.value)}
-              />
-            </div>
-            <div className='mb-6 pt-3 rounded bg-gray-200'>
-              <label className='block text-gray-700 text-sm font-bold mb-2 ml-3'>
-                Right Endcaps
-              </label>
-              <input
-                className='bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3'
-                value={rightEnd}
-                onChange={(event) => setRightEnd(event.target.value)}
-              />
-            </div>
-            <div className='mb-6 pt-3 rounded bg-gray-200'>
-              <label className='block text-gray-700 text-sm font-bold mb-2 ml-3'>
-                Left Endcaps
-              </label>
-              <input
-                className='bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3'
-                value={leftEnd}
-                onChange={(e) => setLeftEnd(e.target.value)}
-              />
-            </div>
-            <div className='mb-6 pt-3 rounded bg-gray-200'>
-              <label className='block text-gray-700 text-sm font-bold mb-2 ml-3'>
-                Inside Corners
-              </label>
-              <input
-                className='bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3'
-                value={insideCorner}
-                onChange={(e) => setInsideCorner(e.target.value)}
-              />
-            </div>
-            <div className='mb-6 pt-3 rounded bg-gray-200'>
-              <label className='block text-gray-700 text-sm font-bold mb-2 ml-3'>
-                Outside Corners
-              </label>
-              <input
-                className='bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3'
-                value={outsideCorner}
-                onChange={(e) => setOutsideCorner(e.target.value)}
-              />
-            </div>
-            <div>
-              <button
-                className='w-full bg-sky-600 hover:bg-indigo-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200'
-                type='submit'
-              >
-                Submit
-              </button>
+          <form onSubmit={calcTotal}>
+            <div className='flex flex-wrap -mx-3 mb-6'>
+              <div className='w-full md:w-1/2 px-3 mb-6'>
+                <label className='block uppercase tracking-widest text-gray-700 text-xs font-bold mb-2'>
+                  Linear Feet
+                </label>
+                <input
+                  className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+                  value={linear}
+                  onChange={(e) => setLinear(e.target.value)}
+                />
+              </div>
+              <div className='w-full md:w-1/2 px-3 mb-6'>
+                <label className='block uppercase tracking-widest text-gray-700 text-xs font-bold mb-2'>
+                  Cost Per
+                </label>
+                <input
+                  className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+                  value={costLinear}
+                  onChange={(e) => setCostLinear(e.target.value)}
+                />
+              </div>
+              <div className='w-full md:w-1/2 px-3 mb-6'>
+                <label className='block uppercase tracking-widest text-gray-700 text-xs font-bold mb-2'>
+                  Right Endcaps
+                </label>
+                <input
+                  className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+                  value={rightEnd}
+                  onChange={(event) =>
+                    setRightEnd(event.target.value)
+                  }
+                />
+              </div>
+              <div className='w-full md:w-1/2 px-3 mb-6'>
+                <label className='block uppercase tracking-widest text-gray-700 text-xs font-bold mb-2'>
+                  Cost Per
+                </label>
+                <input
+                  className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+                  value={costRight}
+                  onChange={(e) => setCostRight(e.target.value)}
+                />
+              </div>
+              <div className='w-full md:w-1/2 px-3 mb-6'>
+                <label className='block uppercase tracking-widest text-gray-700 text-xs font-bold mb-2'>
+                  Left Endcaps
+                </label>
+                <input
+                  className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+                  value={leftEnd}
+                  onChange={(e) => setLeftEnd(e.target.value)}
+                />
+              </div>
+              <div className='w-full md:w-1/2 px-3 mb-6'>
+                <label className='block uppercase tracking-widest text-gray-700 text-xs font-bold mb-2'>
+                  Cost Per
+                </label>
+                <input
+                  className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+                  value={costLeft}
+                  onChange={(e) => setCostLeft(e.target.value)}
+                />
+              </div>
+              <div className='w-full md:w-1/2 px-3 mb-6'>
+                <label className='block uppercase tracking-widest text-gray-700 text-xs font-bold mb-2'>
+                  Inside Corners
+                </label>
+                <input
+                  className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+                  value={insideCorner}
+                  onChange={(e) => setInsideCorner(e.target.value)}
+                />
+              </div>
+              <div className='w-full md:w-1/2 px-3 mb-6'>
+                <label className='block uppercase tracking-widest text-gray-700 text-xs font-bold mb-2'>
+                  Cost Per
+                </label>
+                <input
+                  className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+                  value={costInner}
+                  onChange={(e) => setCostInner(e.target.value)}
+                />
+              </div>
+              <div className='w-full md:w-1/2 px-3 mb-6'>
+                <label className='block uppercase tracking-widest text-gray-700 text-xs font-bold mb-2'>
+                  Outside Corners
+                </label>
+                <input
+                  className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+                  value={outsideCorner}
+                  onChange={(e) => setOutsideCorner(e.target.value)}
+                />
+              </div>
+              <div className='w-full md:w-1/2 px-3 mb-6'>
+                <label className='block uppercase tracking-widest text-gray-700 text-xs font-bold mb-2'>
+                  Cost Per
+                </label>
+                <input
+                  className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+                  value={costOuter}
+                  onChange={(e) => setCostOuter(e.target.value)}
+                />
+              </div>
+              <div className='w-full'>
+                <button
+                  className='w-full bg-sky-600 hover:bg-indigo-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200'
+                  type='submit'
+                >
+                  Submit
+                </button>
+              </div>
             </div>
           </form>
           <div>
